5f3a5decd3f27a4927f3b4176e6df3df128ddd99
yuansheng.sun <785015141@qq.com>
2024-03-27 00:07:30 +0800

    new

--- lidarmap/engine/factory/factory_component.cpp
+++
@@ -1,9 +0,0 @@
-#include"factory_component.h"
-
-namespace YSSLM {
-	namespace engine {
-		namespace factory {
-std::unordered_map<std::string, std::function<std::shared_ptr<NodeComponent>()>> factoryComponent::map_component;
-		}
-	}
-}
--- lidarmap/engine/log_udp/log.h
+++
@@ -1,9 +0,0 @@
-#ifndef _YuanshengSun_LM_Engine_LogUdp_Log_H_
-#define _YuanshengSun_LM_Engine_LogUdp_Log_H_
-
-#include "log_udp.h"
-
-#define _YSSLM_ULOG_STATR_(T , Y)   YSSLM::engine::logudp::CLOG::Initializa(T , Y)
-#define _YSSLM_ULOG_CLOSE_   YSSLM::engine::logudp::CLOG::DeInitializa()
-
-#endif
